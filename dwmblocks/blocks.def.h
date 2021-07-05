000b76655ba085df0f7424e2461859f87fb2e7a8
Chedly Zouche <chedlyzouche9999@outlook.com>
2021-07-05 05:06:31 +0100

    Added new blocks

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,9 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"BAT : ", "acpi | cut -d ':' -f 2- | cut -d ',' -f 2- | cut -c 2-",	60,		0},
-	{"BRI : ", "light -G",	20,		0},
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"", "sensors | head -4 | tail  -1 | cut -c 16- | xargs",	5,		0},
+	{" : ", "acpi | cut -d ':' -f 2- | cut -d ',' -f 2- | cut -c 2-",	60,		0},
+	{" : ", "light -G",	20,		0},
+	{" ", "date '+%a %b %d %I:%M%p'",					5,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
