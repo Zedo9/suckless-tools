c9014fb1dcb073bd9a928d99230f558d96ef201a
Chedly Zouche <chedlyzouche9999@outlook.com>
2021-07-18 09:33:44 +0100

    Changed icon for battery in dwmblocks

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -2,7 +2,7 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "sensors | head -4 | tail  -1 | cut -c 16- | xargs",	5,		0},
-	{" : ", "acpi | cut -d ':' -f 2- | cut -d ',' -f 2- | cut -c 2-",	60,		0},
+	{" : ", "acpi | cut -d ':' -f 2- | cut -d ',' -f 2- | cut -c 2-",	60,		0},
 	{" : ", "light -G",	20,		0},
 	{" ", "date '+%a %b %d %I:%M%p'",					5,		0},
 };
