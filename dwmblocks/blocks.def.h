04ef10ccebe10a5ab222d27bcfcc145513e8ec58
Chedly Zouche <chedlyzouche9999@outlook.com>
2021-10-02 21:20:01 +0100

    Added netspeed block

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,6 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"", "netspeed",	2,		0},
 	{"", "sensors | head -4 | tail  -1 | cut -c 16- | xargs",	5,		0},
 	//{" : ", "acpi | cut -d ':' -f 2- | cut -d ',' -f 2- | cut -c 2-",	60,		0},
 	{" : ", "light -G",	20,		0},
