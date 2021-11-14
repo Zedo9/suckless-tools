b5cac7239ae80f6ed61ec53ac1eabccfa1ddf631
Chedly Zouche <chedlyzouche9999@outlook.com>
2021-11-14 18:11:22 +0100

    Improved cpu temp command in dwmblocks

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -2,7 +2,7 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "netspeed",	2,		0},
-	{"", "sensors | head -4 | tail  -1 | cut -c 16- | xargs",	5,		0},
+	{"", "sensors | grep Tctl | cut -f2 -d'+' | xargs",	5,		0},
 	//{" : ", "acpi | cut -d ':' -f 2- | cut -d ',' -f 2- | cut -c 2-",	60,		0},
 	{" : ", "light -G",	20,		0},
 	{" ", "date '+%a %b %d %I:%M%p'",					5,		0},
