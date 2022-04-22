13d4bc02a98fe94f67218cb91b3026fbca18ae5e
Chedly Zouche <chedlyzouche9999@outlook.com>
2022-04-22 21:28:27 +0100

    Add onedark theme

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -12,18 +12,11 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "CascadiaCode-Medium:size=10:antilias=true:autohint=true" };
-static const char dmenufont[]       = "CascadiaCode-Medium:size=10:antilias=true:autohint=true";
-static const char col_gray1[]       = "#000000";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#ffffff";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-};
+static const char *fonts[]          = { "JetBrainsMono-Bold:size=11:antilias=true:autohint=true" };
+static const char dmenufont[]       = "JetBrainsMono-Bold:size=11:antilias=true:autohint=true";
+
+#include "themes/one-dark.h"
+
 /* tagging */
 /*static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };*/
 /*static const char *tags[] = { "", "", "", "", "", "", "", "", "" };*/
@@ -104,7 +97,7 @@ static Key keys[] = {
 	
 	/* Programs */
 	{ MODKEY,                       XK_w,      spawn,          SHCMD("firefox") },
-	{ MODKEY,                       XK_f,      spawn,          SHCMD("thunar") },
+	{ MODKEY,                       XK_f,      spawn,          SHCMD("nautilus") },
 	{ MODKEY,                       XK_a,      spawn,          SHCMD("skippy-xd") },
 	/* Laptop and Media Keys */
 	{ 0,                       		XF86XK_AudioPlay, spawn, {.v = playstop } },
